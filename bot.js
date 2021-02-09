56d5c9e73e06c75402e21fc315e6b380be1f3f1e
Ryan Klaproth <ryanklaproth@gmail.com>
2021-02-09 12:21:20 -0600

    Twitch Command
    Implemented a command that grabs some of my Twitch account info using node-fetch. Set up command directory/implementation for modular command implementation (just in case).

--- bot.js
+++ bot.js
@@ -1,10 +1,58 @@
 const bot_settings = require("./botsettings.json");
 const discord = require("discord.js");
+const fetch = require("node-fetch"); //Used for Twitch API
+const fs = require("fs"); //fs is Node.js's native file system module
 
 const bot = new discord.Client();
+bot.commands = new discord.Collection();
 
-bot.on("ready", async() => {
-	console.log(`Bot is ready! ${bot.user.tag}`);
+fs.readdir("./commands/", (err, files) => {
+	if(err) console.error(err);
+	
+	//"test.hello.js" = [test.hello.js]. Pop takes last element (js)
+	let js_files = files.filter(file => file.split(".").pop() === "js");
+	
+	if(js_files.length <= 0)
+	{
+		console.log("No commands to load!");
+		return;
+	}
+	
+	console.log(`Loading ${js_files.length} commands!`);
+	
+	js_files.forEach((file, i) =>
+	{
+		let props = require(`./commands/${file}`);
+		console.log(`${i + 1}: ${file} loaded!`);
+		bot.commands.set(props.help.name, props);
+	});
+});
+
+bot.once("ready", () => {
+	console.log(`Bot is ready! ${bot.user.username}`);
+	console.log(bot.commands);	
+});
+
+
+bot.on("ready", async() => {	
+});
+
+bot.on("message", async message => {
+	//Bot/DM Checks
+	if (message.author.bot) return;
+	if (message.channel.type === "dm") return;
+
+	//Storing message to use
+	let messageArr = message.content.split(" ");
+	let args = messageArr.slice(1);
+
+	//Reading stored message to see if it starts with the prefix for a command
+	let command_message = messageArr[0];
+	if (!command_message.startsWith(bot_settings.prefix)) return;
+
+	//Check if the command exists, and run it if it does
+	let command_from_list = bot.commands.get(command_message.slice(bot_settings.prefix.length));
+	if(command_from_list) command_from_list.run(bot, message, args);
 });
 
 bot.login(bot_settings.token);
---
+++ commands/twitch.js
@@ -0,0 +1,41 @@
+const bot_settings = module.require("../botsettings.json");
+const discord = require("discord.js");
+const fetch = require("node-fetch"); //Used for Twitch API
+
+//For grabbing Twitch API data
+const userURL = 'https://api.twitch.tv/helix/users?login=kappylp';
+const headers = {
+	'Authorization':'Bearer '+ bot_settings.twitch_token,
+	'Client-ID':'hvkh2ps0jcndeha05urm1uy4j0nu5k',
+}
+
+//JUST ME [KAPPY]
+module.exports.run = async (bot, message, args) => {
+	//Testing with user api call
+	fetch(userURL, {
+			headers: headers,
+		}).then(r => r.json())
+		.then(body => {
+
+		let data = body.data;
+		let notifChannel = message.guild.channels.cache.get('556936544682901512');
+		
+		let embed = new discord.MessageEmbed()
+			.setAuthor(data[0].display_name)
+			.setDescription(data[0].broadcaster_type)
+			.addField("User ID", data[0].id, true)
+			.addField("View Count", data[0].view_count, true)
+			.setThumbnail(data[0].profile_image_url)
+			.setFooter("Type: " + data[0].type);
+		
+		if(!notifChannel) message.channel.send("@here HELLO", {embed: embed});
+		else notifChannel.send ("@here HELLO", {embed: embed});
+			
+		//console.log(body.data[0].id);
+	}).catch((err) => console.log("Caught " + err.stack));
+}
+
+
+module.exports.help = {
+	name: "twitch"
+}
