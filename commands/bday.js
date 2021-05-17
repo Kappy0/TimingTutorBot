df3c4dfb06027c9c610c7dfb2600b44f11b82b88
Ryan Klaproth <ryanklaproth@gmail.com>
2021-05-17 08:21:33 -0500

    Pull winners command
    Command to pull winners' names implemented. Not sure if nesting the SQL statements is the best way to do it, but will try to optimize it later.
    
    Also, planning to add a third command for users to use to list the people who guessed Pass or Fail.

--- commands/bday.js
+++ commands/bday.js
@@ -42,6 +42,68 @@ module.exports.run = async (bot, message, args, connection) => {
 	}
 
 	//Second command will allow me to grab all winners and list them
+	if(args[0] === "winners")
+	{
+		if(args[1] === "pass")
+		{
+			let numWinners;
+
+			connection.query(`SELECT COUNT(*) as count FROM bday WHERE guess = 'Pass'`, (err, rows) => {
+				if(err) logger.log("[" + date(new Date()).toISOString() + "] " + err);
+
+				numWinners = rows[0].count;
+				console.log(numWinners);
+			
+				connection.query(`SELECT * FROM bday WHERE guess = 'Pass'`, (err, rows) => {
+					if(err) logger.log("[" + date(new Date()).toISOString() + "] " + err);
+
+					let msg = "__**WINNERS**__\n";
+
+					let i = 0;
+					while(i < numWinners)
+					{
+						msg += rows[i].name + "\n";
+						i++;
+					}
+
+					if(msg === "__**WINNERS**__\n") msg += "None";
+
+					message.channel.send(msg);
+				});
+			});
+		}
+		else if(args[1] === "fail")
+		{
+			let numWinners;
+
+			connection.query(`SELECT COUNT(*) as count FROM bday WHERE guess = 'Fail'`, (err, rows) => {
+				if(err) logger.log("[" + date(new Date()).toISOString() + "] " + err);
+
+				numWinners = rows[0].count;
+				console.log(numWinners);
+			
+				connection.query(`SELECT * FROM bday WHERE guess = 'Fail'`, (err, rows) => {
+					if(err) logger.log("[" + date(new Date()).toISOString() + "] " + err);
+
+					let msg = "__**WINNERS**__\n";
+
+					let i = 0;
+					while(i < numWinners)
+					{
+						msg += rows[i].name + "\n";
+						i++;
+					}
+
+					if(msg === "__**WINNERS**__\n") msg += "None";
+
+					message.channel.send(msg);
+				});
+			});
+		}
+	}
+
+	//3rd command lists all users who guessed pass or fail
+	
 }
 
 module.exports.help = {
