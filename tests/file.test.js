ca88400108411df9a9dfa10e56ec5790caed5ed1
Ryan Klaproth <ryanklaproth@gmail.com>
2021-06-22 23:07:02 -0500

    Setting up read tests

---
+++ tests/file.test.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const fileUtils = require('../file.js');
+
+describe('testing reading files', () => {
+	test('reads file data', () => {
+
+	});
+
+	test('reads empty file', () => {
+
+	});
+});
