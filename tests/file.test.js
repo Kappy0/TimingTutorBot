71b61137533ae105432d326fc4424090721af830
Kappy0 <ryanklaproth@gmail.com>
2021-06-23 02:43:48 -0500

    Setting up tests for writing to files
    Tests are now up for writing to files asynchronously and synchronously.

--- tests/file.test.js
+++ tests/file.test.js
@@ -1,28 +1,42 @@
 const fs = require("fs");
 const fileUtils = require('../file.js');
 
-const mockJSON = {some_id: "abcdef", another_id: "ghijkl"};
+const mockReadJSON = {some_id: "abcdef", another_id: "ghijkl"};
 
 describe('testing reading files', () => {
 	test('reads file data asynchronously', async () => {
-		let mockData = await fileUtils.readAsync('./tests/mock.json');
+		const mockData = await fileUtils.readAsync('./tests/mockRead.json');
 
-		expect(mockData).toStrictEqual(mockJSON);
+		expect(mockData).toStrictEqual(mockReadJSON);
 	});
 
 	test('reads file data synchronously', () => {
-		let mockData = fileUtils.readSync('./tests/mock.json');
+		const mockData = fileUtils.readSync('./tests/mockRead.json');
 
-		expect(mockData).toStrictEqual(mockJSON);
+		expect(mockData).toStrictEqual(mockReadJSON);
 	});
 });
 
 describe('testing writing to files', () => {
 	test('writes to files asynchronously', async () => {
+		const number = Math.random();
+		const mockWriteJSON = {some_id: number};
 
+		await fileUtils.writeAsync('./tests/mockWrite.json', mockWriteJSON);
+
+		let mockData = fileUtils.readSync('./tests/mockWrite.json');
+
+		expect(mockData.some_id).toBe(number);
 	});
 
 	test('writes to files synchronously', () => {
+		const number = Math.random();
+		const mockWriteJSON = {another_id: number};
 
+		fileUtils.writeSync('./tests/mockWrite.json', mockWriteJSON);
+
+		let mockData = fileUtils.readSync('./tests/mockWrite.json');
+
+		expect(mockData.another_id).toBe(number);
 	});
 });
